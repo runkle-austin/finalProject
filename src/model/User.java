e1f4da4bc43d9016f72d410d226ef3193fb7a69d
runkle-austin <166458209+runkle-austin@users.noreply.github.com>
2025-04-05 13:47:12 -0700

    Add files via upload

--- src/model/User.java
+++ src/model/User.java
@@ -1,9 +1,24 @@
+// User.java
 package model;
 
+// This class stores all of the information about a user
 public class User {
 	// INSTANCE VARIABLES
+	private String userName;
+	private String password;
 	
-	// 
+	// CONSTRUCTOR
+	public User(String name, String pw) {
+		this.userName = name;
+		this.password = pw;
+	}
 	
-	// 
+	// METHODS
+	public String getUserName() {
+		return this.userName;
+	}
+	
+	public String getPassword() {
+		return this.password;
+	}
 }
