c557dcbe549106f41cb9fc47dfed828d8968b061
Austin Runkle <apr4@arizona.edu>
2025-04-07 13:45:29 -0700

    plz work

--- src/model/WorkoutCycle.java
+++ src/model/WorkoutCycle.java
@@ -1,16 +1,24 @@
 package model;
 
+import java.time.DayOfWeek;
 import java.util.HashMap;
 
 public final class WorkoutCycle {
 	private String name;
 	private int numberWeeks;
-	private HashMap fullCycle;
+	private HashMap<DayOfWeek, Workout> fullCycle = new HashMap<DayOfWeek, Workout>();
 	
 	public WorkoutCycle(String name, int numberWeeks) {
 		this.name = name;
 		this.numberWeeks = numberWeeks;
 	}
+	
+	// METHOD
+	public String getName() {
+		return this.name;
+	}
+	
+	// 
 	
 	
 }
