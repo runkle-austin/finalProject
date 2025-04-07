053f2ab811cf311d057ee4c92cd8309f8736819a
Austin Runkle <apr4@arizona.edu>
2025-04-07 13:34:24 -0700

    pair

---
+++ src/tests/WorkoutCycleTest.java
@@ -0,0 +1,14 @@
+package tests;
+
+import org.junit.Test;
+
+import model.WorkoutCycle;
+
+public class WorkoutCycleTest {
+	
+	@Test
+	public void testAddWorkout() {
+		WorkoutCycle cw = new WorkoutCycle("Test1", 5);
+		
+	}
+}
