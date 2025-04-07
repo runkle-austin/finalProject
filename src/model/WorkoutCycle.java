feb05e2b0db7e9866c450c6935045975b1664b6b
carlossilva23 <carlossilva23@arizona.edu>
2025-04-06 17:07:04 -0700

    Added WorkoutCycle class and changed text files

---
+++ src/model/WorkoutCycle.java
@@ -0,0 +1,16 @@
+package model;
+
+import java.util.HashMap;
+
+public final class WorkoutCycle {
+	private String name;
+	private int numberWeeks;
+	private HashMap fullCycle;
+	
+	public WorkoutCycle(String name, int numberWeeks) {
+		this.name = name;
+		this.numberWeeks = numberWeeks;
+	}
+	
+	
+}
