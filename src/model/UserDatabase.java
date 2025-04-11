02b1e7d76c8a20a2ccd2f674b42f1e2ee0cc2a46
carlossilva23 <carlossilva23@arizona.edu>
2025-04-11 15:58:16 -0700

    Hashing Pair Programming with Austin and Ryan

--- src/model/UserDatabase.java
+++ src/model/UserDatabase.java
@@ -8,13 +8,14 @@ import java.io.IOException;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 import java.security.SecureRandom;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Scanner;
 
 // Code retrieved from LA2 UserDatabase
 public class UserDatabase {
 	// hashmap with user name as the key and User as the value
-	private HashMap<String, User> accounts = new HashMap<String, User>();
+	private ArrayList<User> accounts = new ArrayList<>();
 	
 	// load the current users in the file accounts.txt
 	public void loadDatabase() throws FileNotFoundException {
@@ -31,8 +32,8 @@ public class UserDatabase {
 
 			// TODO generate salt
 			
-			// add the user to the map of users
-			accounts.put(accountInfo[0], curUser);			
+			// add the user to the ArrayList of users
+			accounts.add(curUser);			
 		}
 		
 		scanner.close();
@@ -41,10 +42,13 @@ public class UserDatabase {
 	
 	// add user to the database, returns true if successfully creates, false otherwise
 	public boolean addUser(String username, String password) {
-		if (accounts.containsKey(username)) {
-			return false;
+		for (User user : accounts) {
+			byte[] salt = user.getSalt();
+			String encodedName = encode(username, salt);
+			if (encodedName == user.getUserName()) {
+				return false;
+			} 
 		}
-
 		//generate a salt for the password
 		SecureRandom random = new SecureRandom();
 		byte[] salt = new byte[64];
@@ -53,17 +57,17 @@ public class UserDatabase {
 		
 		// encrypt the password
 		String encodedPassword = encode(password, salt);
+		String encodedUsername = encode(username, salt);
 		
-		//
-		User newUser = new User(username, encodedPassword);
+		User newUser = new User(encodedUsername, encodedPassword);
 		// add the salt to the user
 		newUser.setSalt(salt);
-		// add the user to the hashMap of users
-		accounts.put(username,newUser);
+		// add the user to the ArrayList of users
+		accounts.add(newUser);
 		
 		// write the new user to User.txt
 		try(FileWriter fw = new FileWriter("Users.txt", true)){
-			fw.write(username + "," + encodedPassword + "," + byteToHex(salt)+"\n");
+			fw.write(encodedUsername + "," + encodedPassword + "," + byteToHex(salt)+"\n");
 		} catch (IOException e) {
 			System.out.println("Error writing to file");
 		}
@@ -73,39 +77,33 @@ public class UserDatabase {
 	
 	// determine if the entered user name and password match the expected information
 	public User login(String username, String pass) {
-		
-		if (accounts.containsKey(username)) {
-			// get the users encoded password and salt
-			String password = accounts.get(username).getPassword();
-			byte[] salt = accounts.get(username).getSalt();
-			//encode the entered password
-			String encodedPW = encode(pass, salt);
-			// check if the passwords are the same
-			if (password.equals(encodedPW)) {
-				// if they are the same then return the 
-				return accounts.get(username);
+		for (User user : accounts) {
+			byte[] salt = user.getSalt();
+			String encodedName = encode(username, salt);
+			if (encodedName == user.getUserName()) {
+				return user;
 			}
 		}
 		return null;
 	}
 	
 	// encodes the given string
-	public String encode(String password, byte[] salt) {
+	public String encode(String input, byte[] salt) {
 		//
 		try {
 			// sha256 encodes using SHA-256
 			MessageDigest sha256 = MessageDigest.getInstance("SHA-256");
-			String encodedPassword = "";
+			String encodedStr = "";
 			//add the salt and password to sha256
-			sha256.update(password.getBytes());
+			sha256.update(input.getBytes());
 			sha256.update(salt);
 			//hash the password
 			byte[] hash = sha256.digest();
 			//convert the hash into a string
 			for (byte b : hash) {
-				encodedPassword += String.format("%02x", b);
+				encodedStr += String.format("%02x", b);
 			}
-			return encodedPassword;
+			return encodedStr;
 
 		} catch (NoSuchAlgorithmException e) {
 			System.out.println("Error with encode");
