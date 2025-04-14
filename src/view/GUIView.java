1e8ef773aee2f7256383c53c57f4a220e2a2b20f
AmeyaMad <ameya.mad@gmail.com>
2025-04-13 21:23:28 -0700

    Added basic GUI using JavaFX. Should be useable by teammates

---
+++ src/view/GUIView.java
@@ -0,0 +1,25 @@
+package view;
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+public class GUIView extends Application {
+
+    @Override
+    public void start(Stage stage) {
+        Label label = new Label("🎉 JavaFX is working!");
+        StackPane root = new StackPane(label);
+        Scene scene = new Scene(root, 300, 200);
+
+        stage.setTitle("GUIView Test Window");
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
