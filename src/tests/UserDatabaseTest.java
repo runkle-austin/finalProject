389d7f9a686e37c58db98641618832a864c8ff74
carlossilva23 <carlossilva23@arizona.edu>
2025-04-18 14:49:43 -0700

    UserDatabase functionality

--- src/tests/UserDatabaseTest.java
+++ src/tests/UserDatabaseTest.java
@@ -5,6 +5,8 @@ package tests;
 import org.junit.jupiter.api.Test;
 import model.UserDatabase;
 
+import static org.junit.Assert.assertEquals;
+
 public class UserDatabaseTest {
 
 
@@ -23,7 +25,25 @@ public class UserDatabaseTest {
         UserDatabase db = new UserDatabase();
 		db.addUser("a", "b");
 		db.saveToJSONFile();
+		assertEquals(db.addUser("a", "c"), false);
 	}
 
+	@Test
+	public void testLogin() {
+		UserDatabase db = new UserDatabase();
+		db.addUser("a", "b");
+		db.saveToJSONFile();
+
+	}
+
+	@Test
+	public void testPassword() {
+		UserDatabase db = new UserDatabase();
+		assertEquals(db.isStrongPassword("A!b2%s;lkjsdhdssjsdh"), "");
+		assertEquals(db.isStrongPassword("A!b"), "Needs to be more than eight letters\n");
+		assertEquals(db.isStrongPassword("Abcdjdjdjdj"), "Missing special character\n");
+		assertEquals(db.isStrongPassword("!!!!"), "Missing capital character\nMissing lower character\nNeeds to be more than eight letters\n");
+		assertEquals(db.isStrongPassword(""), "Missing special character\nMissing capital character\nMissing lower character\nNeeds to be more than eight letters\n");
+	}
 	
 }
